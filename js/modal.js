bc1a94e4ff3993ec165a821eddad55dca48d9082
Yuri <iurii_bardych@hotmail.com>
2022-12-16 23:06:35 +0100

    updates 1

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,8 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    checkbox: document.querySelector("[data-modal-сheckbox]"),
+    checkboxlabel: document.querySelector("[data-modal-сheckbox-label]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +12,12 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+  }
+
+  refs.checkbox.addEventListener("click", toggleCheckbox);
+  refs.checkboxlabel.addEventListener("click", toggleCheckbox);
+
+  function toggleCheckbox() {
+    refs.checkbox.classList.toggle("checked");
   }
 })();
